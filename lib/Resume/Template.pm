9b0dbec32fe2a2aad487cd285ab77d30e16597d3
jason.a.may <jason.a.may@gmail.com>
2008-06-11 23:42:16 -0400

    I now use Ubuntu and OpenBSD
    darcs-hash:20080612034216-20c2c-8d702a50e3ac8392dbf9d5fa255691d8b335137f.gz

--- lib/Resume/Template.pm
+++ lib/Resume/Template.pm
@@ -56,7 +56,7 @@ my @sections = (
                     ul {
                         li {
                             "*nix: ".
-                            "Gentoo 2007.0, Debian (Etch, Lenny), FreeBSD 6.2" }
+                            "Gentoo 2007.0, Debian (Etch, Lenny), Ubuntu, FreeBSD 6.x, OpenBSD 4.x" }
                     }
                 }
             ],
