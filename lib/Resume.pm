8cb713bf9b3ce8534cc4b9ba208068ce9e55293c
Jason May <jason.a.may@gmail.com>
2009-05-11 15:56:04 -0400

    get a version ready

--- lib/Resume.pm
+++ lib/Resume.pm
@@ -6,6 +6,8 @@ use Template::Declare;
 use Resume::Template;
 Template::Declare->init(roots => ['Resume::Template']);
 
+our $VERSION = '0.01';
+
 sub generate {
     print Template::Declare->show('wrap');
 }
